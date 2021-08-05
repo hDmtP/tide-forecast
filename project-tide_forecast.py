76f73d2037a52ac3f3f475ca7b3a66f8f3c8de8a
Pritam Dhara <pritamdhara914@gmail.com>
2021-08-05 22:08:11 +0530

    added GUI

--- project-tide_forecast.py
+++ project-tide_forecast.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
-import html5lib
+import tkinter as tk
+
 
 def main():
     URL = "https://tide-forecast.com/locations/Calcutta-Garden-Reach-India/tides/latest"
@@ -10,13 +10,28 @@ def main():
     tide_forecast = soup.find(class_="tide-day-tides")
 
     count = 0
+    arr = [None]*25
+
+    root = tk.Tk()
+
+    root.geometry("512x300")
+
+    root.minsize(256, 128)
+    root.maxsize(1024, 512)
+
+    photo = tk.PhotoImage(file="tide_forecast.png")
+
     for elem1 in tide_forecast.stripped_strings:
-        print(elem1)
+        arr[count] = elem1
         count += 1
-        if(count>=4 and count%4==0):
-            print("\n")
+        if(count >= 4 and count % 4 == 0):
+            pass
 
+    vadim_label = tk.Label(
+        root, text=f"{arr[4]} = {arr[5]}\n{arr[9]} = {arr[10]}\n{arr[14]} = {arr[15]}\n{arr[19]} = {arr[20]}", image=photo, compound="top")
+    vadim_label.pack()
 
+    root.mainloop()
 
 
 if __name__ == "__main__":
